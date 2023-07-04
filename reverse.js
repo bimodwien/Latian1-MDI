774b588908d58acb1764b0d575302387f3ac7a75
Bimo Dwien Prabowo <bimodwien2@gmail.com>
2023-07-04 09:30:25 +0700

    latian1

---
+++ reverse.js
@@ -0,0 +1,21 @@
+const array1 = [1,2,3,4,5,6,7]
+const reverse1 = []
+
+
+for (let i = array1.length - 1; i >=0; i--) {
+    const value1 = array1[i]
+    // console.log(value)
+    reverse1.push(value1)    
+}
+
+console.log(reverse1)
+
+const array2 = [4,5,2]
+const reverse2 = []
+
+for (let i = array2.length - 1; i >= 0; i--) {
+    const value2 = array2[i]
+    reverse2.push(value2)
+}
+
+console.log(reverse2)
